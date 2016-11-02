bda07f7de27de3ee906915144951e4e043e774ec
tpei <t.s.peikert@gmail.com>
2016-11-02 15:57:57 +0100

    env file load bug fix

--- config/sequel_connect.rb
+++ config/sequel_connect.rb
@@ -1,6 +1,9 @@
 require 'sequel'
+require 'dotenv'
 
-db_uri ||= ENV['DB_URI'] ||
+Dotenv.load
+
+db_uri = ENV['DB_URI'] ||
     'postgres://127.0.0.1:5432/postgres?user=postgres'
 
 DB = Sequel.connect(db_uri, max_connections: 10)
